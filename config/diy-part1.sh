dd7968cec1c0246d10215510e30ba73a2bc12793
AJ <983097239@qq.com>
2024-12-26 11:33:18 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,3 +1,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git small-package https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git small-package https://github.com/kenzok8/small-package' >>feeds.conf.default
